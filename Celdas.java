ee6ba756e83ecab803c34ac10bef774d6741133f
kfelsner <kristofersolerfelsner@protonmail.com>
2017-02-14 09:27:54 +0100

    Clase que contiene datos sobre las celdas.

---
+++ Celdas.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package buscaminas;
+
+/**
+ *
+ * @author kfelsner
+ */
+public class Celdas {
+    public boolean revelada = false;
+    public boolean mina = false;
+    public int numero;
+    public char forma = '#';
+    
+}
